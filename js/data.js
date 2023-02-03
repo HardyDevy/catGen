09487b2b950b2c52c18944af1381d165336318a7
HardyDevy <dghardy2020@gmail.com>
2023-02-03 15:02:49 -0500

    Writes js and basic html

---
+++ js/data.js
@@ -0,0 +1,191 @@
+const dataText = [
+    {
+        "text": "Damn all the jellyfish"
+    },
+    {
+        "text": "i do the opposite of gatekeeping. you will be forced to like my interests. you will listen"
+    },
+    {
+        "text": "im so proud of you"
+    },
+    {
+        "text": "I'm moving to Las Vegas."
+    },
+    {
+        "text": "please stop leaving me on read. we are soul mates and you are missing out on really fun conversations"
+    },
+    {
+        "text": "wawawa wawawawawoo"
+    },
+    {
+        "text": "meow"
+    },
+    {
+        "text": "I wonder what Mona Lisa was thinking about while she was painted"
+    },
+    {
+        "text": " *fist bump* "
+    },
+    {
+        "text": "im a super villain"
+    },
+    {
+        "text": "good morning i hope ur day is good"
+    },
+    {
+        "text": "they should make cigarettes without the cancer i think"
+    },
+    {
+        "text": "rush hour > pulp fiction"
+    },
+    {
+        "text": "i missed groundhogs day"
+    },
+    {
+        "text": "litter box broke"
+    },
+    {
+        "text": "gotta do my taxes"
+    },
+    {
+        "text": "business, business, business, numbers, numbers.."
+    },
+    {
+        "text": "I'm italian"
+    },
+    {
+        "text": "I've never seen an epsiode of the simpsons"
+    },
+    {
+        "text": "It's Franky Friday! (My name is Franky)"
+    },
+    {
+        "text": "ummm... ok"
+    },
+    {
+        "text": "pineapple pizza >>>>>>>>"
+    },
+    {
+        "text": "i cannot afford a PS5"
+    },
+    {
+        "text": "bring back among us"
+    },
+    {
+        "text": "i love ice cubes"
+    },
+    {
+        "text": "what is wrong with you"
+    },
+    {
+        "text": "im perfect"
+    },
+    {
+        "text": "i ball hard"
+    },
+    {
+        "text": "im gonna be the next kevin hart"
+    },
+    {
+        "text": "arrggghh (im a pirate)"
+    },
+    {
+        "text": "maybe the fbi and cia are just misunderstood and actually cool guys"
+    },
+    {
+        "text": "Please defund the police."
+    }
+]
+
+const dataImg = [
+    {
+        "img": "cat1.jpeg"
+    },
+    {
+        "img": "cat2.jpeg"
+    },
+    {
+        "img": "cat3.jpeg"
+    },
+    {
+        "img": "cat4.jpeg"
+    },
+    {
+        "img": "cat5.jpeg"
+    },
+    {
+        "img": "cat6.jpeg"
+    },
+    {
+        "img": "cat7.jpeg"
+    },
+    {
+        "img": "cat8.jpeg"
+    },
+    {
+        "img": "cat9.jpeg"
+    },
+    {
+        "img": "cat10.jpg"
+    },
+    {
+        "img": "cat11.jpeg"
+    },
+    {
+        "img": "cat12.jpeg"
+    },
+    {
+        "img": "cat13.jpeg"
+    },
+    {
+        "img": "cat14.jpg"
+    },
+    {
+        "img": "cat15.jpeg"
+    },
+    {
+        "img": "cat16.jpeg"
+    },
+    {
+        "img": "cat17.jpeg"
+    },
+    {
+        "img": "cat18.jpg"
+    },
+    {
+        "img": "cat19.jpeg"
+    },
+    {
+        "img": "cat20.jpg"
+    },
+    {
+        "img": "cat21.jpeg"
+    },
+    {
+        "img": "cat22.jpeg"
+    },
+    {
+        "img": "cat23.jpeg"
+    },
+    {
+        "img": "cat24.jpg"
+    },
+    {
+        "img": "cat25.jpeg"
+    },
+    {
+        "img": "cat26.jpeg"
+    },
+    {
+        "img": "cat27.jpg"
+    },
+    {
+        "img": "cat28.jpeg"
+    },
+    {
+        "img": "cat29.jpeg"
+    },
+    {
+        "img": "cat30.png"
+    }
+]
--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-document.getElementById('blue').style.color = 'blue' 
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    
+    let i = Math.floor(Math.random() * dataText.length);
+    let quote = document.querySelector('blockquote');
+    quote.innerText = dataText[i].text;
+
+    let i2 = Math.floor(Math.random() * dataImg.length);
+    let img = document.querySelector('img');
+    img.src = 'img/Pictures of Cats/' + dataImg[i2].img;
+
+});
